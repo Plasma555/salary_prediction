bf57ebadbbcec58daa36ab2e454c4e85dad835c2
Asouk <126856438+Plasma555@users.noreply.github.com>
2023-06-14 21:57:43 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,11 @@
-
 import numpy as np
 from flask import Flask, request, jsonify, render_template
 import pickle
 import pandas as pd
 
 app = Flask(__name__)#name of directory
-model = pickle.load(open('model.pkl','rb'))
-model_columns=pickle.load(open('model_columns.pkl','rb'))
+model = pickle.load(open(r'C:\Users\asouk\Desktop\salary_prediction-main\model.pkl','rb'))
+model_columns=pickle.load(open(r'C:\Users\asouk\Desktop\salary_prediction-main\model_columns.pkl','rb'))
 
 #route
 @app.route('/')
@@ -21,6 +20,10 @@ def predict():
     query_ = pd.get_dummies(pd.DataFrame(json_, index = [0]), prefix=['Gender','Job Title','Education Level'], columns=['Gender','Job Title','Education Level'])
 
     query = query_.reindex(columns = model_columns, fill_value= 0)
+    
+    query["Age"] = pd.to_numeric(query["Age"], errors="coerce")
+    query["Years of Experience"] = pd.to_numeric(query["Years of Experience"], errors="coerce")
+
     prediction=list(model.predict(query))
     output = round(prediction[0],2)
 
